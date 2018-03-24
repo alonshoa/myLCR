1e994b7314dee895157ecb93e83a0ca8b850ae98
Alon Shoa <shoa.alon@gmail.com>
2018-03-24 13:27:05 +0300

    new project commit with gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myLCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
